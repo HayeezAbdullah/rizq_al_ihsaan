d1c471444d692367130e9765d49d5e75ffd6dc85
Hayeez Abdullah <hayeezabdullah2k5@gmail.com>
2025-02-27 01:23:59 +0530

    Customize Designs

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,16 @@ const App = () => {
   return (
     <div className="bg-[#F6F6F6] min-h-screen">
       <Navbar />
-      <div className="flex mx-5  px-8 py-6">
-        <div className="flex flex-col gap-4 w-[25%]">
+      <div className="flex flex-col md:flex-row mx-5 px-6 py-6 gap-6">
+        {/* Cards Section */}
+        <div className="flex flex-col gap-4 w-full md:w-[25%]">
           <Card title="Total Families" number="1,234" />
           <Card title="Total Students" number="57" />
           <Card title="Current Treasury" number="$12,345" />
         </div>
 
-        <div className="w-[75%] mx-5 my-2">
+        {/* Graph Section */}
+        <div className="w-full md:w-[75%]">
           <GraphCard />
         </div>
       </div>
--- src/Card.jsx
+++ src/Card.jsx
@@ -3,11 +3,11 @@ import { GoLocation } from "react-icons/go";
 const Card = ({ title, number }) => {
   return (
     <div className="bg-white shadow-lg w-full h-[133px] rounded-xl p-5 flex flex-col justify-between relative border border-gray-200 hover:shadow-xl transition-shadow duration-300">
-      <GoLocation className="absolute right-4 top-4 text-gray-600 text-2xl" />
+      <GoLocation className="absolute right-4 top-4 text-red-600 text-2xl hidden lg:block" />
 
       <p className="text-gray-600 text-lg font-medium">{title}</p>
 
-      <p className="text-4xl font-bold text-gray-700">{number}</p>
+      <p className="text-5xl font-bold text-gray-700">{number}</p>
 
       <div className="absolute bottom-0 left-0 w-full h-1 bg-gray-300 rounded-b-xl"></div>
     </div>
